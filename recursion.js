362d347817cfcde58a07c93a02620a4ed6c27c70
Andrew Dutson <Dutsandrew@gmail.com>
2022-08-21 09:52:03 -0600

    fibonacci sequence code runs

--- recursion.js
+++ recursion.js
@@ -1,1 +1,23 @@
-console.log('hey');
+function fibs(number) {
+  let fibSequence = [];
+  let firstNumber = 0;
+  let secondNumber = 1;
+  let sum = 0;
+  for (let i = 0; i < number - 1; i++) {
+    if (i == 0) {
+      fibSequence.push(firstNumber, secondNumber);
+    } else {
+      sum = firstNumber + secondNumber;
+      fibSequence.push(sum);
+      firstNumber = secondNumber;
+      secondNumber = sum;
+    }
+  }
+  return fibSequence;
+}
+
+console.log(fibs(8));
+
+// function fibsRec(number) {
+
+// }
